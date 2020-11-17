ac7b9f72caaad4353274e0f53a52647dae57317c
NataliaRajska <natalia.rajskaa@gmail.com>
2020-11-17 14:35:08 +0100

    Add object with properties

---
+++ app.js
@@ -0,0 +1,26 @@
+const team = {
+    _players: [
+        {
+            firstName: 'Jacek',
+            lastName: 'Dylu',
+            age: 31
+        },
+        {
+            firstName: 'Natalia',
+            lastName: 'Raj',
+            age: 30
+        }
+    ],
+    _games: [
+        {
+            opponent: 'Love',
+            teamPoints: 42,
+            opponentPoints: 27
+        },
+        {
+            opponent: 'Peace',
+            teamPoints: 40,
+            opponentPoints: 12
+        }
+    ],
+};
