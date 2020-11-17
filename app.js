b10f4e349cb070f2b60f23216f370aa9d9b8dae5
NataliaRajska <natalia.rajskaa@gmail.com>
2020-11-17 15:32:54 +0100

    Add games and players

--- app.js
+++ app.js
@@ -42,12 +42,25 @@ const team = {
             age: age
         }
         this._players.push(player);
-    }
+    },
 
+    addGame(opponentName, tPoints, oppPoints) {
+        let game = {
+            opponent: opponentName,
+            teamPoints: tPoints,
+            opponentPoints: oppPoints
+        }
+        this._games.push(game);
+    }
 };
 
 team.addPlayer('Steph', 'Curry', 28);
 team.addPlayer('Lisa', 'Leslie', 44);
 team.addPlayer('Bugs', 'Bunny', 76);
 
+team.addGame('Heart', 22,44);
+team.addGame('Lips', 22,44);
+team.addGame('Smart', 22,44);
 
+console.log(team._players);
+console.log(team._games);
