4f5ae3369e7855de7b0aa351ae556a39a21c0c74
NataliaRajska <natalia.rajskaa@gmail.com>
2020-11-17 14:54:17 +0100

    Invoke team object

--- app.js
+++ app.js
@@ -21,19 +21,33 @@ const team = {
             opponent: 'Peace',
             teamPoints: 40,
             opponentPoints: 12
+        },
+        {
+            opponent: 'Hugs',
+            teamPoints: 22,
+            opponentPoints: 23
         }
     ],
+    get players() {
+        return this._players;
+    },
+    get games() {
+        return this._games;
+    },
     //=========================
     addPlayer(firstName, lastName, age) {
-        let player = {
+        const player = {
             firstName: firstName,
             lastName: lastName,
             age: age
         }
-        this._players.push(player)
+        this._players.push(player);
     }
 
 };
 
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
 
 
